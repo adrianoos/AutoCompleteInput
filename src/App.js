fe76dd227a1fc320760c1e9c1ecf431bd747dbde
Adrian <adriann.kowalski@gmail.com>
2021-06-10 10:06:08 +0200

    added user and filteredUser state, useEffect to fetch data on page load, and filter functions

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
-import React from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
 
+  const [ users, setUsers ] = useState([])
+  const [ filteredUsers, setFilteredUsers ] = useState([])
+
   const getData = async () => {
-  const response = await axios.get(`https://jsonplaceholder.typicode.com/users`)
-  console.log(response)
-  }
+  const { data } = await axios.get(`https://jsonplaceholder.typicode.com/users`)
+  setUsers(data)
+  };
 
- getData()
+  useEffect(() => {
+   getData()
+  }, [])
 
+ const filterUsers = (e) => {
+   const inputText = e.currentTarget.value
+   const filteredUsers = getFilteredUsers(inputText)
+   setFilteredUsers(filteredUsers)
+ };
+
+ const getFilteredUsers = (inputText) => {
+    return users.filter(item => item.username.toLowerCase().includes(inputText.toLowerCase()))
+ };
+
+ console.log(filteredUsers)
 
   return (
     <div id='MainContainer'>
+        <h2>Search by UserName</h2>
         <form id='SubmitForm'>
-           <input id='Input' placeholder='UserName' type='text' ></input>
+           <input id='Input' placeholder='UserName' type='text' onInput={filterUsers}></input>
            <button id='SubmitButton' type='submit'>SUBMIT</button>
         </form>
     </div>
